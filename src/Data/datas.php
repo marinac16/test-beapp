ac813f8f04e2c5ce2e5ca3e84979b4d1806305e2
Marina <cormerais10@gmail.com>
2021-01-09 18:59:43 +0100

    mise en place des données

--- src/Data/datas.php
+++ src/Data/datas.php
@@ -7,27 +7,45 @@ namespace App\Data;
 use App\Entity\Status;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class datas
 {
-    public function load(ObjectManager $manager){
+
+    /**@var UserPasswordEncoderInterface */
+    private $encoder;
+
+    /**@var ObjectManager */
+    private $manager;
+
+    public function __construct(UserPasswordEncoderInterface $encoder, ObjectManager $manager)
+    {
+        $this->encoder = $encoder;
+        $this->manager = $manager;
+    }
+
+    /**
+     */
+    public function load(){
 
         //Status datas creation
         $statusAble = new Status();
         $statusAble->setName("Able");
-        $manager->persist($statusAble);
+        $this->manager->persist($statusAble);
 
         $statusDisable = new Status();
         $statusDisable->setName("Disable");
-        $manager->persist($statusDisable);
+        $this->manager->persist($statusDisable);
 
         //User datas creation
         $user = new User();
         $user->setEmail("admin@email.fr");
-        $user->setPassword("admin");
+        $hash = $this->encoder->encodePassword($user, "admin");
+        $user->setPassword($hash);
         $user->setRoles(["ADMIN"]);
+        $this->manager->persist($user);
 
-        $manager->flush();
+        $this->manager->flush();
 
     }
 
