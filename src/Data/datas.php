867217612e7bbb667c94ae48cbee10e1a4ca445c
Marina <cormerais10@gmail.com>
2021-01-09 18:10:31 +0100

    User entity

--- src/Data/datas.php
+++ src/Data/datas.php
@@ -5,6 +5,7 @@ namespace App\Data;
 
 
 use App\Entity\Status;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 
 class datas
@@ -19,6 +20,12 @@ class datas
         $statusDisable = new Status();
         $statusDisable->setName("Disable");
         $manager->persist($statusDisable);
+
+        //User datas creation
+        $user = new User();
+        $user->setEmail("admin@email.fr");
+        $user->setPassword("admin");
+        $user->setRoles(["ADMIN"]);
 
         $manager->flush();
 
